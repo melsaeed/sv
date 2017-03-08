df60951f862515202b54c37c1be6728f9fd20ccb
m.elsaeed <m.elsaeed@192.168.1.10>
2017-03-08 21:13:56 +0200

    updates

---
+++ platforms/browser/www/js/base64.js
@@ -0,0 +1,5 @@
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
--- www/app/controllers/upload_images.js
+++ www/app/controllers/upload_images.js
@@ -3,23 +3,21 @@ safa.controller("upload_imagesCtrl",function($scope, $location, $http){
     $scope.user = JSON.parse(window.localStorage.getItem('user'));
     $scope.ready = false;
 
-    $scope.start = function() {
-        //make a payload
-        
-        
         payload = {
             id: $scope.user.company_id + '.' + $scope.user.branch_id,
             token_key: md5(md5($scope.user.company_id + '.' + $scope.user.branch_id))
         };
+        
+    $scope.start = function() {
+        //make a payload
+        
+        
 
         $http({
             url: 'http://sync.safavisa.com/api/v1/cases',
             method: 'get',
             params: {payload: JSON.stringify(payload)}
         }).then(function(res){
-            //if(res.data.errors)
-            //popup.message('×طأ', res.data.errors);
-
             $scope.cases = res.data.data;
         });
 
@@ -47,9 +45,33 @@ safa.controller("upload_imagesCtrl",function($scope, $location, $http){
     $scope.send = function() {
         
         
+                console.log($scope.form.case);
+
+//        $http({
+//            url: 'http://sync.safavisa.com/api/v1/mutamers/group/' + $scope.form.group.Id,
+//            method: 'post',
+//            params: {payload: JSON.stringify(payload)}
+//        }).then(function (res) {
+//
+//
+//        
+//        console.log(res.data);
+//       
+//        });
+        
+        //{"Id":1,"Name":"moshtaha","UASP":1,"MOFAUser":"Moin mushtaha","MOFAPassword":"mushtaha@123@","username":"ea17970","password":"ea17970","export_offline":1,"server_id":0,"updated_date_time":"2017-01-28 14:04:36"}
+                
+//        $http({
+//            url: 'http://offline.safavisa.com/MOFA/check_remote_login/'+$scope.form.num,
+//            method: 'post',
+//            data: $.param({token: window.localStorage.getItem("token"), contract: b64EncodeUnicode(JSON.stringify($scope.form.contract))}),
+//            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+//        }).then(function(res){
+//            $scope.image = (res.data.file['0']);
+//        });
         
         
-       
+        
         
     };
     
